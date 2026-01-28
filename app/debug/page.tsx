8a563e4e01bd38bba19b4184fbdeb1a3a0bd75d8
Cristiano Silva <cristiano.godoi@gotmail.com>
2026-01-28 16:38:08 -0300

    chore: add debug page

---
+++ app/debug/page.tsx
@@ -0,0 +1,8 @@
+export default function DebugPage() {
+    return (
+        <div className="p-10 text-center">
+            <h1 className="text-4xl text-green-600 font-bold">DEBUG WORKS</h1>
+            <p className="mt-4">If you can see this, deployment is working correctly.</p>
+        </div>
+    )
+}
